a8342c6f41cacc4f87d75bfef32a285ee331f780
Ibrahim Semary <92101864+ibrahimsemary@users.noreply.github.com>
2023-11-27 09:38:25 -0600

    added personal website

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("Professional").onclick = () => {
+    location.href = "/Professional.html"
+}
+document.getElementById("Organizations").onclick = () => {
+    location.href = "/Organizations.html"
+}
+document.getElementById("AboutMe").onclick = () => {
+    location.href = "/AboutMe.html"
+}
+document.getElementById("Contact").onclick = () => {
+    location.href = "/Contact.html"
+}
+document.getElementById("Home").onclick = () => {
+    location.href = "/"
+}
